d7e23f4bd3db3d94f00839d042c1eb259a9da567
a5ur4 <pedroarthurbastosmoreira2999@gmail.com>
2025-01-19 23:41:45 -0300

    Add PDF text extraction functionality using pdf2image and Tesseract

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 import os
 import pytesseract
 import cv2
-# Add the pdf2image library to transform all the pages of the PDF into images
+from pdf2image import convert_from_path
 
-image_path = r"" # Insert the path to the image you want to extract text from
-
+folder_path = r"C:\Users\pedro\Downloads\curriculums"
 path_tesseract = r"C:\Users\pedro\AppData\Local\Programs\Tesseract-OCR"
 pytesseract.pytesseract.tesseract_cmd = os.path.join(path_tesseract, "tesseract.exe")
+poppler_path = r"C:\poppler\Library\bin"
 
-def extract_text_from_image(image_path):
-    image = cv2.imread(image_path)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    text = pytesseract.image_to_string(gray)
-    return text
-
-text = extract_text_from_image(image_path)
-print(text)
+for filename in os.listdir(folder_path):
+    if filename.endswith(".pdf"):
+        file_path = os.path.join(folder_path, filename)
+        
+        images = convert_from_path(file_path, poppler_path=poppler_path)
+        
+        for i, image in enumerate(images):
+            image_path = f"{filename}_page_{i}.png"
+            image.save(image_path, "PNG")
+            
+            text = pytesseract.image_to_string(cv2.imread(image_path))
+            print(text)
+            os.remove(image_path)
+            
+        print(f"Analisando: {filename}")
